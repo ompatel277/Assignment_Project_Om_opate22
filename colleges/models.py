8d7dcae539a75e9eb0e4b37ec68626b1346c081f
ompatel277 <ompatel277@gmail.com>
2025-09-16 23:07:48 -0500

    week3 assignment changes

--- colleges/models.py
+++ colleges/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# create the class name College and the column names
+class College(models.Model):
+    college_name = models.CharField(max_length=300) # full college name
+    city = models.CharField(max_length=100) # full city name
+    state = models.CharField(max_length=100) # full state name
+    abbreviation = models.CharField(max_length=50, unique=True) # ex: UIUC
+    logo_url = models.URLField(blank = True, null = True) # logo of college
