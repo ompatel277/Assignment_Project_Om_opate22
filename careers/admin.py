586578438533e056020e947088a56e9c3cb41a96
ompatel277 <ompatel277@gmail.com>
2025-09-16 23:51:45 -0500

    changes

--- careers/admin.py
+++ careers/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Career
 
-# Register your models here.
+admin.site.register(Career)
--- careers/models.py
+++ careers/models.py
@@ -1,8 +1,8 @@
-from django .db import models
+from django.db import models
 
 class Career(models.Model):
     position = models.CharField(max_length=100)
-    skill_sets = ....
+   #skill_sets = ###
     title = models.CharField(max_length=100)
     industries = models.CharField(max_length=100)
     description = models.TextField()
