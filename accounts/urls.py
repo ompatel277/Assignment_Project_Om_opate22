651eaa350d0bfc302ee52034f5f0d7063bf32174
ompatel277 <ompatel277@gmail.com>
2025-09-23 18:51:09 -0500

    View 3 http

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path("users/http/", views.user_list_http, name="user_list_http"),
     path("users/render/", views.user_list_render, name="user_list_render"),
-    path("", views.user_list_render, name="home"),  # homepage → render view
 ]
