d5a6672192f53c9c0a425e5fbc3fe512337510ee
ompatel277 <ompatel277@gmail.com>
2025-09-23 18:49:09 -0500

    View 2 http

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 from accounts.models import User
 
 #Https
-def user_list(request):
+def user_list_http(request):
     user = User.objects.all()
     template = loader.get_template('accounts/user_list.html')
     context = {"user": user}
     output = template.render(context, request)
     return HttpResponse(output)
 
+# render
+def user_list_render(request):
+    user = User.objects.all()
+    return render(request, 'accounts/user_list.html', {'user': user})
 
 
