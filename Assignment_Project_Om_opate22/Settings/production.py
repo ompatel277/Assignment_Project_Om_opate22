be31274321eea1b80545a1b6f51930570469837a
Om Patel <ompat@MacBook-Air-242.local>
2025-11-22 16:43:44 -0600

    changes to production

--- Assignment_Project_Om_opate22/Settings/production.py
+++ Assignment_Project_Om_opate22/Settings/production.py
@@ -1,6 +1,9 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'opate22.pythonanywhere.com']
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Assignment_Project_Om_opate22.Settings.production'
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'opate22.pythonanywhere.com'
+]
