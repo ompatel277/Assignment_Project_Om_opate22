9d171d4a820fc48dd5b86d343972e24dd62db07c
Om Patel <ompat@MacBook-Air-242.local>
2025-11-24 22:10:04 -0600

    production.py fix

--- Assignment_Project_Om_opate22/settings/production.py
+++ Assignment_Project_Om_opate22/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import os
 
 DEBUG = False
 
@@ -7,3 +8,30 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'opate22.pythonanywhere.com'
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://opate22.pythonanywhere.com'
+]
+
+# -------------------
+# DATABASE (SQLite)
+# -------------------
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
+    }
+}
+
+# -------------------
+# STATIC FILES
+# -------------------
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# -------------------
+# MEDIA FILES
+# -------------------
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
