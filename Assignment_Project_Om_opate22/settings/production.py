797fb0138f8ecfc2ded534c8e8c14240867dd5fb
Om Patel <ompat@wirelessprv-10-194-165-210.near.illinois.edu>
2025-12-02 11:16:24 -0600

    fix

--- Assignment_Project_Om_opate22/settings/production.py
+++ Assignment_Project_Om_opate22/settings/production.py
@@ -6,12 +6,11 @@ ALLOWED_HOSTS = ['opate22.pythonanywhere.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': "opate22$info390gc-mysql-database-Traject-opate22",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'opate22$info390gc-mysql-database-Traject-opate22',
         'USER': 'opate22',
         'PASSWORD': 'Crispyunicorn6248$',
         'HOST': 'opate22.mysql.pythonanywhere-services.com',
         'PORT': '3306',
-
     }
 }
